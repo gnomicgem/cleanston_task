af340279c9e8db3f5e2af75ed118f10ebefa445a
Daniel <der.einzige.dan@gmail.com>
2025-04-02 20:37:01 +0700

    Discount slider improved

--- app/javascript/controllers/discount_slider_controller.js
+++ app/javascript/controllers/discount_slider_controller.js
@@ -1,55 +1,50 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["amount"]
+    static targets = ["discountAmount"]
 
     connect() {
-        this.update({ target: this.element });
+        this.debouncedSubmit = this.debounce(this.submitForm.bind(this), 200);
     }
 
     update(event) {
-        const discount = event.target.value
-        this.amountTarget.textContent = `${discount}₽`
-        console.log("update: discount =", discount, "amountTarget.textContent =", this.amountTarget.textContent)
+        const discount = event.target.value;
+
+        const amountElement = document.querySelector("#discount-amount");
+        if (!amountElement) return;
+
+        // Анимация исчезновения
+        amountElement.classList.add("fade-out");
+
+        setTimeout(() => {
+            amountElement.textContent = `${discount} ₽`;
+            amountElement.classList.remove("fade-out");
+            amountElement.classList.add("fade-in");
+
+            setTimeout(() => {
+                amountElement.classList.remove("fade-in");
+
+                this.debouncedSubmit();
+
+            }, 100);
+        }, 100);
+    }
+
+    debouncedSubmit() {
+        const form = this.element.closest("form");
+        if (form) {
+            clearTimeout(this.timeout);
+            this.timeout = setTimeout(() => {
+                form.requestSubmit();
+            }, 100);
+        }
+    }
+
+    debounce(func, wait) {
+        let timeout;
+        return function (...args) {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => func.apply(this, args), wait);
+        };
     }
 }
-
-
-
-
-
-
-
-
-// import { Controller } from "@hotwired/stimulus"
-// import { patch } from "@rails/request.js"
-//
-// export default class extends Controller {
-//     static targets = ["amount", "slider", "total"]
-//
-//     update(event) {
-//         const discount = event.target.value
-//         this.amountTarget.textContent = `${discount} ₽`
-//         console.log("update: discount =", discount, "amountTarget.textContent =", this.amountTarget.textContent);
-//         this.sliderTarget.value = discount;
-//
-//         this.updateDiscount(discount);
-//     }
-//
-//     updateDiscount(discount) {
-//         if (this.element.dataset.cartId) {
-//             patch(`/carts/${this.element.dataset.cartId}`, {
-//                 body: { discount: discount },
-//                 responseKind: "json"
-//             }).then(response => {
-//                 response.json.then(data => {
-//                     this.totalTarget.textContent = `${data.total_price} ₽`
-//                     this.sliderTarget.value = discount;
-//                     console.log("updateDiscount: discount =", discount, "totalTarget.textContent =", this.totalTarget.textContent);
-//                 })
-//             })
-//         } else {
-//             console.error("cartId is not defined");
-//         }
-//     }
-// }
